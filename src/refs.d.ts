0d1a883dad9cc1474bcc03fc08bd3c8c262126aa
Daniel Jackson <daniel@cloakedninjas.co.uk>
2018-12-01 23:02:48 +0000

    Prepping prod build

--- src/refs.d.ts
+++ src/refs.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../node_modules/phaser-ce/typescript/phaser.comments.d.ts" />
+
+//grunt-start
+/// <reference path="scripts/entity/food.ts" />
+/// <reference path="scripts/entity/preload-bar.ts" />
+/// <reference path="scripts/game.ts" />
+/// <reference path="scripts/state/game.ts" />
+/// <reference path="scripts/state/preloader.ts" />
+//grunt-end
